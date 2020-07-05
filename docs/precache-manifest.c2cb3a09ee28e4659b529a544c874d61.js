230c149c80d9c163bf8c96d42bbf32350eabc851
Marcel <halanocca21@gmail.com>
2020-07-05 11:15:07 -0500

    add docs

---
+++ docs/precache-manifest.c2cb3a09ee28e4659b529a544c874d61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e11fe0f8efd8b45ae29e65f91c7e009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a51f421cecb3960b9e7d",
+    "url": "/static/css/main.81333c20.chunk.css"
+  },
+  {
+    "revision": "027ae5944804431f3db5",
+    "url": "/static/js/2.9f575afa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f575afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a51f421cecb3960b9e7d",
+    "url": "/static/js/main.4e85c437.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
